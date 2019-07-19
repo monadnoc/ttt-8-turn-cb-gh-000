003b43b078bda114c4eae6b6b368583b01451dcb
Jordan Nelson <jordannelson1.2014@u.northwestern.edu>
2019-07-19 20:26:23 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,44 @@
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  ind = input_to_index(input)
+  until valid_move?(board, ind)
+    print "Please enter 1-9:"
+  end
+  # move(board, ind)
+  # display_board(board)
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, ind)
+    if position_taken?(board, ind)
+      return false
+    elsif ind.between?(0, 8)
+      return true
+    end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, ind)
+  if board[ind] == " " || board[ind] == "" || board[ind] == nil
+    return false
+  else
+    return true
+  end
+end
+
+def input_to_index(inp)
+  return inp.to_i - 1
+end
+
+def move(b, inp, char="X")
+  b[inp] = char
+  return b
+end
